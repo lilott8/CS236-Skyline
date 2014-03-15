1a7521e9b200f5028b06b385faf30380ea16958a
lilott8 <hidden>
2014-03-14 22:35:02 -0700

    Laying the groundwork to implement the naive approach

--- src/main/java/Reduce.java
+++ src/main/java/Reduce.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by jason on 3/2/14.
@@ -12,16 +13,24 @@ public class Reduce extends Reducer<IntWritable, Weather, LongWritable, Text> {
     private LongWritable one = new LongWritable();
     //private IntWritable two = new IntWritable();
     private Text two = new Text();
+    private ArrayList<Weather> skyline = new ArrayList<Weather>();
 
     public void reduce(IntWritable key, Iterable<Weather> weather, Context context)
             throws IOException, InterruptedException {
-        for (Weather w : weather) {
+        for (Weather wOuter : weather) {
+            for (Weather wInner : weather) {
+ 
+            }
+        }
+         /*
+         for(Weather w : weather) {
             one.set(w.getKey());
             int tid = context.getTaskAttemptID().getId();
             two.set(Integer.toString(w.getStation()) + "\t" + tid);
 
             context.write(one, two);
-        }
+         }*/
         //context.write(one, text);
     }
+
 }
